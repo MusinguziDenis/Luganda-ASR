8df01fbf8148770e429ef792aec7c1210b82822f
Denis Musinguzi <denismusinguzi2511@gmail.com>
2025-02-09 01:04:37 +0100

    Updated the code with language model

--- finetuning_wav2vec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Run multi parallel the fine-tuning using 4 GPUs.
-# All options are writen in finetuning_common_voice.json.
-
-python -m torch.distributed.launch --nproc_per_node=4 finetuning_wav2vec.py  finetuning_wav2vec.json
